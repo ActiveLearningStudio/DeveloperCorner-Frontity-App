0232ada3e1e7298e225ab38fee876af394af7c35
aqibnwaz <aqib@curriki.org>
2021-12-01 16:54:32 +0500

    design cleaning

--- packages/mars-theme/src/containers/generaldiscussion/archives.js
+++ packages/mars-theme/src/containers/generaldiscussion/archives.js
@@ -1,35 +1,22 @@
 import React from "react";
-// import Headings from 'curriki-design-system/dist/utils/Headings/headings';
 const Archives = () => {
   return (
     <>
       <div className="archives-section">
-        {/* <Headings headingType="h2" color="#084892" text="ARCHIVES" /> */}
         <h2>ARCHIVES</h2>
         <div className="archives-link">
-          {/* <Headings className="spase" headingType="body3" color="#084892" text="April 2021" />
-     <Headings className="spase" headingType="body3" color="#084892" text="February 2021" />
-     <Headings className="spase" headingType="body3" color="#084892" text="December 2020" />
-     <Headings className="spase" headingType="body3" color="#084892" text="November 2020" />
-     <Headings className="spase" headingType="body3" color="#084892" text="September 2020" />
-     <Headings className="spase" headingType="body3" color="#084892" text="August 2020" />
-     <Headings className="spase" headingType="body3" color="#084892" text="July 2020" />
-     <Headings className="spase" headingType="body3" color="#084892" text="June 2020" />
-     <Headings className="spase" headingType="body3" color="#084892" text="March 2020" />
-     <Headings className="spase" headingType="body3" color="#084892" text="November 2019" />
-     <Headings className="spase" headingType="body3" color="#084892" text="September 2019" />
-     <Headings className="spase" headingType="body3" color="#084892" text="April 2019" /> */}
           <a>April 2021</a>
           <a>February 2021</a>
-          <a>May 2021</a>
-          <a>August 2021</a>
-          <a>June 2021</a>
-          <a>July 2021</a>
-          <a>Nov 2021</a>
-          <a>Decemebr 2021</a>
-          <a>May 2021</a>
-          <a>January 2021</a>
-          <a>April 2020</a>
+          <a>December 2020</a>
+          <a>November 2020</a>
+          <a>September 2020</a>
+          <a>August 2020</a>
+          <a>July 2020</a>
+          <a>June 2020</a>
+          <a>March 2020</a>
+          <a>November 2019</a>
+          <a>September 2019</a>
+          <a>April 2019</a>
         </div>
       </div>
     </>
