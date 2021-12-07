95a27c8b16fdad332cb02638c3ab40fd1823fc26
aqibnwaz <aqib@curriki.org>
2021-12-07 18:16:40 +0500

    privacy policy design fix cleaning

--- packages/mars-theme/src/containers/PolicyPrivacy/heading.js
+++ packages/mars-theme/src/containers/PolicyPrivacy/heading.js
@@ -16,7 +16,7 @@ function Heading({ Scrollspy, state }) {
             "headings8",
             "headings9",
             "headings10",
-            "headings12",
+            "headings11",
           ]}
           style={{ padding: 0 }}
           currentClassName="is-current"
@@ -25,42 +25,42 @@ function Heading({ Scrollspy, state }) {
             <div className="heading">
               <p>1.</p>
 
-              <p>Scope of this privacy policy</p>
+              <p className="heading-text">Scope of this privacy policy</p>
             </div>
           </a>
           <a href="#headings2">
             <div className="heading">
               <p>2. </p>
 
-              <p>Information we collect</p>
+              <p className="heading-text">Information we collect</p>
             </div>
           </a>
           <a href="#headings3">
             <div className="heading">
               <p>3.</p>
 
-              <p>How we use the information we collect</p>
+              <p className="heading-text">How we use the information we collect</p>
             </div>
           </a>
           <a href="#headings4">
             <div className="heading">
               <p>4.</p>
 
-              <p>How we share the information we collect</p>
+              <p className="heading-text">How we share the information we collect</p>
             </div>
           </a>
           <a href="#headings5">
             <div className="heading">
               <p>5.</p>
 
-              <p>Your Choices</p>
+              <p className="heading-text">Your Choices</p>
             </div>
           </a>
           <a href="#headings6">
             <div className="heading">
               <p>6.</p>
 
-              <p>Other</p>
+              <p className="heading-text">Other</p>
             </div>
           </a>
         </Scrollspy>
