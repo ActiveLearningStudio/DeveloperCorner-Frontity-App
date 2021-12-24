69c0f55af848ca031a5764a276450bf79bb6f873
aqibnwaz <aqib@curriki.org>
2021-12-24 16:49:32 +0500

    Edit headerMenu into headerPage cleaning

--- packages/mars-theme/src/components/Header/header.js
+++ packages/mars-theme/src/components/Header/header.js
@@ -2,15 +2,28 @@ import React from "react";
 import { Global, css, connect } from "frontity";
 import bootstrapCss from "bootstrap/dist/css/bootstrap.min.css";
 import { Button } from "react-bootstrap";
-import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { Navbar, Container, Nav, NavDropdown, Dropdown } from "react-bootstrap";
 import Brand from "../../assets/images/Logos/Currikistudio-logo.png";
 import usericon from "../../assets/images/UserCircle1.png";
-import heartIcon from '../../assets/images/Vector.png';
+import heartIcon from "../../assets/images/Vector.png";
+import dropdownImg from "../../assets/images/dropdownImg.png";
 import Link from "./link";
 import HeaderCss from "./styles.css";
 const Header = ({ state }) => {
   console.log(state.theme.menu);
-  console;
+  const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+    <a
+      href=""
+      ref={ref}
+      onClick={(e) => {
+        e.preventDefault();
+        onClick(e);
+      }}
+    >
+      {/* Render custom icon here */}
+      {children}
+    </a>
+  ));
   return (
     <>
       <Global styles={css(HeaderCss)} />
@@ -26,42 +39,60 @@ const Header = ({ state }) => {
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav>
                 <div className="centered-menu">
-                  <NavDropdown
-                    title="Products"
-                    id="collasible-nav-dropdown"
-                    color="#285AA5"
-                  >
-                    {state.theme.menu.map(([name, link]) => {
-                      const isCurrentPage = state.router.link === link;
-                      return (
-                        <NavDropdown.Item key={name}>
-                          <Link
-                            link={link}
-                            aria-current={isCurrentPage ? "page" : undefined}
-                          >
-                            {name}
-                          </Link>
-                        </NavDropdown.Item>
-                      );
-                    })}
-                  </NavDropdown>
-                  <NavDropdown title="Customers" id="collasible-nav-dropdown">
-                    <NavDropdown.Item href="/providers">
-                      Providers
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                  <NavDropdown title="Community" id="collasible-nav-dropdown">
-                    <NavDropdown.Item href="#">
-                      Bussiness Product
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#">Separated link</NavDropdown.Item>
-                  </NavDropdown>
-                  <NavDropdown title="Company" id="collasible-nav-dropdown">
-                    <NavDropdown.Item href="/providers">
-                      Providers
-                    </NavDropdown.Item>
-                  </NavDropdown>
+                  <Dropdown>
+                    <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-1">
+                      <p>Products</p>
+                      <img src={dropdownImg} />
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu className="super-colors">
+                      {state.theme.menu.map(([name, link]) => {
+                        const isCurrentPage = state.router.link === link;
+                        return (
+                          <Dropdown.Item key={name}>
+                            <Link
+                              link={link}
+                              aria-current={isCurrentPage ? "page" : undefined}
+                            >
+                              {name}
+                            </Link>
+                          </Dropdown.Item>
+                        );
+                      })}
+                    </Dropdown.Menu>
+                  </Dropdown>{" "}
+                  <Dropdown>
+                    <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-1">
+                      <p>Customers</p>
+                      <img src={dropdownImg} />
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu className="super-colors">
+                      <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+                      <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+                      <Dropdown.Item eventKey="3">Separated link</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>{" "}
+                  <Dropdown>
+                    <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-1">
+                      <p>Commmunity</p>
+                      <img src={dropdownImg} />
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu className="super-colors">
+                      <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+                      <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+                      <Dropdown.Item eventKey="3">Separated link</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>{" "}
+                  <Dropdown>
+                    <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-1">
+                      <p>Company</p>
+                      <img src={dropdownImg} />
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu className="super-colors">
+                      <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+                      <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+                      <Dropdown.Item eventKey="3">Separated link</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>{" "}
                 </div>
                 <Nav.Link href="#deets" className="header-login">
                   <img src={usericon} alt="user icon" />
@@ -71,9 +102,9 @@ const Header = ({ state }) => {
 
                 <Button className="donate-btn" variant="primary">
                   <div>
-                  <img src={heartIcon} alt=""/>
+                    <img src={heartIcon} alt="" />
                   </div>
-                 <span>Donate</span> 
+                  <span>Donate</span>
                 </Button>
               </Nav>
             </Navbar.Collapse>
